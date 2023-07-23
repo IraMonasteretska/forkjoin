e81ce9420cbac197537d4ac30d387de268a2b8c5
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2023-07-23 19:37:46 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -71,8 +71,60 @@ $(document).ready(function () {
 
 
 
+    // OUR PROJECTS----------------------------------------------------------------
+    
+    var windowWidth = $(document).width(),
+        containerWidth = $('.container').width(),
+        paddingContainer = ((windowWidth - containerWidth) / 2)
+    $('.projectswelcomesect__logo').css('right', paddingContainer)
 
 
+    $(window).resize(function () {
+        var windowWidth = $(document).width(),
+            containerWidth = $('.container').width(),
+            paddingContainer = ((windowWidth - containerWidth) / 2)
+        $('.projectswelcomesect__logo').css('right', paddingContainer)
+    });
+
+
+
+    $('.customselect').select2({
+        minimumResultsForSearch: -1,
+        // dropdownCssClass: "headerselectdropdown"
+    });
+
+    $(".projsidebox ul").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top - 105 }, 1500);
+    });
+
+    const links = document.querySelectorAll('.projsidebox li a');
+    window.addEventListener('scroll', () => {
+        const anchors = document.querySelectorAll('.projectcontent__wrap [id]');
+        anchors.forEach(anchor => {
+            if (anchor.closest('.projectcontent__wrap')) {
+                const anchorTop = anchor.getBoundingClientRect().top + window.pageYOffset;
+                if (window.pageYOffset >= anchorTop - 110) {
+                    links.forEach(link => {
+                        const correspondingLink = document.querySelector(`.projsidebox li a[href="#${anchor.id}"]`);
+                        if (correspondingLink) {
+                            link.classList.remove('active');
+                            correspondingLink.classList.add('active');
+                        }
+                    });
+                }
+            }
+        });
+    });
+
+    // SERVICE----------------------------------------------------------------
+
+    $('.accordeon__header').click(function(){
+        $(this).toggleClass('open');
+        $(this).next('.accordeon__body').slideToggle();
+    });
 
 
 
